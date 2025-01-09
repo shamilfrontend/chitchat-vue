0731a5480fedc24cb1b831ae3b81f7edde0bfcf0
Alisultanov Shamil <Shamil.Alisultanov@transitcard.ru>
2025-01-10 00:51:29 +0300

    feat: update

--- frontend/components/messenger/left-sidebar.vue
+++ frontend/components/messenger/left-sidebar.vue
@@ -1,35 +1,33 @@
 <template>
-  <!-- Main Chat Left Sidebar start -->
-  <aside class="chitchat-left-sidebar left-disp">
-    <RecentChat />
-    <Favourite />
-    <Documents />
-    <ContactList />
-    <Notifications />
-    <Settings />
-    <Status />
-  </aside>
-  <!-- Main Chat Left Sidebar end -->
+    <aside class="chitchat-left-sidebar left-disp">
+        <RecentChat/>
+        <Favourite/>
+        <Documents/>
+        <ContactList/>
+        <Notifications/>
+        <Settings/>
+        <Status/>
+    </aside>
 </template>
 
 <script>
-import Status from "./lefsidebar/status.vue";
-import Settings from "./lefsidebar/settings.vue";
-import Notifications from "./lefsidebar/notification.vue";
-import ContactList from "./lefsidebar/contact_list.vue";
-import Documents from "./lefsidebar/document.vue";
-import Favourite from "./lefsidebar/favourite.vue";
 import RecentChat from "./lefsidebar/recentchat.vue";
+import Favourite from "./lefsidebar/favourite.vue";
+import Documents from "./lefsidebar/document.vue";
+import ContactList from "./lefsidebar/contact_list.vue";
+import Notifications from "./lefsidebar/notification.vue";
+import Settings from "./lefsidebar/settings.vue";
+import Status from "./lefsidebar/status.vue";
 
 export default {
-  components: {
-    Status,
-    Settings,
-    Notifications,
-    ContactList,
-    Documents,
-    Favourite,
-    RecentChat,
-  },
+    components: {
+        RecentChat,
+        Favourite,
+        Documents,
+        ContactList,
+        Notifications,
+        Settings,
+        Status,
+    },
 };
 </script>
